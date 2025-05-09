1f10633f761c8fe95fdfd46c68bf9130f23a2151
Bryan José <bryanfreitasmg@gmail.com>
2025-05-08 21:46:50 -0300

    💡Dia 26 – Métodos personalizados em Produto

---
+++ day026/src/Produto.java
@@ -0,0 +1,67 @@
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    //construtor
+
+    public Produto(String nome, double preco, int quantidade){
+
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    //getters
+    public String getNome(){
+        return nome;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    //setter
+
+    public void setNome(String nome){
+        if(nome != null && !nome.trim().isEmpty()) {
+            this.nome = nome;
+        }else{
+            System.out.println("Nome inválido");
+        }
+    }
+
+    public void setPreco(double preco) {
+        if(preco > 0) {
+            this.preco = preco;
+        }else{
+            System.out.println("Preco inválido");
+        }
+    }
+
+    public void setQuantidade(int quantidade) {
+        if(quantidade >= 0) {
+            this.quantidade = quantidade;
+        }else{
+            System.out.println("Quantidade inválida");
+        }
+    }
+
+
+    public void aumentarEstoque(int quantidade){
+        if(quantidade > 0){
+            this.quantidade += quantidade;
+        }else {
+            System.out.println("Quantidade inválida para adicionar ao estoque.");
+        }
+    }
+
+    public void mostrarDados(){
+        System.out.println("Produtos: " + nome + " | Preco " + preco + " | Quantidade: " + quantidade);
+    }
+}
